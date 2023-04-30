b593f08d46ad827d6e456407fe05555fe9a1bc9e
HussamAltalib <hussamaltalib@outlook.com>
2023-04-30 08:34:31 +0300

    create store app and product models

--- OnlineShop/main/views.py
+++ OnlineShop/main/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
+from store.models import Product
 
 # Create your views here.
 
 def index(request : HttpRequest):
-   return  render(request, "main/index.html")
+   products = Product.objects.all().filter(is_available=True)
+
+   context = {
+      'products': products,
+   }
+
+   return  render(request, "main/index.html", context)
